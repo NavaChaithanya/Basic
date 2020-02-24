3a7a75c3fd68781fa786babf3e3c83f91fd09a14
nava <nava.chaithu@hotmail.com>
2020-02-24 21:47:46 +0530

    empty commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import App1 from './App1';
+
+function App() {
+    return (
+        <App1/>
+    );
+}
+
+export default App;
---
+++ src/App1.js
@@ -0,0 +1,9 @@
+import React from "react";
+const App1 = props => {
+  return (
+    <div>
+      <p> hello world </p>{" "}
+    </div>
+  );
+};
+export default App1;
